48ddb8441c309675ff5ccdcc7ef275d05cb97fe2
Abdi Osman <aosman@c2r10s6.42wolfsburg.de>
2025-06-02 17:23:58 +0200

    4

--- include/libft.h
+++ include/libft.h
@@ -12,5 +12,6 @@ size_t  ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int ft_isdigit(int c);
 int	ft_isalnum(int c);
+int ft_isascii(int c);
 
 #endif
--- src/ft_alnum.c
+++ src/ft_alnum.c
@@ -6,7 +6,7 @@
 /*   By: aosman <aosman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 17:06:21 by aosman            #+#    #+#             */
-/*   Updated: 2025/06/02 17:09:53 by aosman           ###   ########.fr       */
+/*   Updated: 2025/06/02 17:18:44 by aosman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,15 @@
 
 int main(void)
 {
-    // Test cases for ft_isdigit
-    char test1 = '5';
-    char test2 = '=';
-    char test3 = 'A';
-    char test4 = '9';
-    char test5 = ' ';
-
-    printf("ft_isalnum('%c') = %d\n", test1, ft_isalnum(test1)); // Expected: 1
-    printf("ft_isalnum('%c') = %d\n", test2, ft_isalnum(test2)); // Expected: 0
-    printf("ft_isalnum('%c') = %d\n", test3, ft_isalnum(test3)); // Expected: 1
-    printf("ft_isalnum('%c') = %d\n", test4, ft_isalnum(test4)); // Expected: 1
-    printf("ft_isalnum('%c') = %d\n", test5, ft_isalnum(test5)); // Expected: 0
-
+    printf("%d", 'C');
     return 0;
 }
 
 
+
 int	ft_isalnum(int c)
 {
-    if (ft_isalpha(c) || ft_isdigit(c))
+   if (ft_isalpha(c) || ft_isdigit(c))
         return (1);
     return (0);
 }
